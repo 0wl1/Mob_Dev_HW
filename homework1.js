5e5751872eb793cc2426ef98fa3309ea448bdcc3
Owl <staneniteksenia@gmail.com>
2024-03-27 20:25:50 +0200

    homework1

---
+++ homework1.js
@@ -0,0 +1,10 @@
+const firstName = "Ксения";
+const lastName = "Станёните";
+const middleName = "робертовна";
+const yearOfBirth = "1998";
+const currentYear = new Date().getFullYear();
+let age = 0;
+const studying = false; //0
+const Working = true; // 1
+age = currentYear - parseInt(yearOfBirth);
+console.table({firstName, lastName, middleName, yearOfBirth, currentYear, age, studying, Working})
