3f0a32d792fa9c8b304ff7bdebd146ebaeddd19d
Owl <staneniteksenia@gmail.com>
2024-03-30 10:23:16 +0200

    code update2

--- homework2.js
+++ homework2.js
@@ -3,6 +3,7 @@ const arrayTestText1 = "быть или не быть -";
 const arrayTestText2 = " вот в чем вопрос.";
 function mult(value1, value2) {
     testFunctionReturn1 = value1 + value2;
+
     console.log("№1:", testFunctionReturn1);
 }
 mult(arrayTestText1, arrayTestText2);
@@ -12,6 +13,7 @@ const arrayTestNumber1 = [1,2,3,4,5];
 const arrayTestNumber2 = [10,20,30];
 function all(_value3, _value4, _value5) {
   var merge1 = Array.prototype.join.call(arguments);
+
   console.log("№2:", merge1);
 }
 all(testFunctionReturn2, arrayTestNumber1, arrayTestNumber2);
@@ -21,12 +23,15 @@ function reverseString(_value6, _value7) {
   var splitString = merge2.split("");
   var reverseArray = splitString.reverse(); // только переворачивает двузначные числа тоже
   var joinArray = reverseArray.join("");
+
   console.log("№3:", joinArray);
+
   return joinArray;
   }
 reverseString(arrayTestNumber1, arrayTestNumber2);
 
 testFunctionReturn1 = testFunctionReturn1.replace(testFunctionReturn1[0], testFunctionReturn1[0].toUpperCase());
+
 console.log("№4:", testFunctionReturn1);
 
 function random(min, max) {
@@ -34,5 +39,7 @@ function random(min, max) {
 }
 const min=1;
 const max=100;
+
 let randomInt = random(min, max);
+
 console.log("№5: Случайное число между " + min + " и " + max + " = " + randomInt);
